144722fd1661caa80e29d66de521fba2258a2d16
alassman <alassman@umich.edu>
2016-06-17 10:47:18 -0400

    testing

--- Archer.cpp
+++ Archer.cpp
@@ -34,8 +34,8 @@ void Archer::make_connection() {
 	if(status != RQ_SUCCESS)
 	{
 		cout<<"Error connecting to device..."<<endl;
-		cout << "Running ./test_MC_connection 2 times" << endl;
-		for (int i = 0; i < 2; ++i)
+		cout << "Running ./test_MC_connection 5 times" << endl;
+		for (int i = 0; i < 5; ++i)
 		{
 			system("./test_MC_connection");
 			usleep(1000);
