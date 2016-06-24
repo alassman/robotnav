0d3c4084c7928c9b552a5811f40ea3d00912c307
alassman <alassman@umich.edu>
2016-06-24 15:23:12 -0400

    testing

---
+++ apriltags/pod-build/CMakeFiles/apriltags.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/barton/Documents/aptags/apriltags/src/Edge.cc" "/home/barton/Documents/aptags/apriltags/pod-build/CMakeFiles/apriltags.dir/src/Edge.cc.o"
+  "/home/barton/Documents/aptags/apriltags/src/FloatImage.cc" "/home/barton/Documents/aptags/apriltags/pod-build/CMakeFiles/apriltags.dir/src/FloatImage.cc.o"
+  "/home/barton/Documents/aptags/apriltags/src/GLine2D.cc" "/home/barton/Documents/aptags/apriltags/pod-build/CMakeFiles/apriltags.dir/src/GLine2D.cc.o"
+  "/home/barton/Documents/aptags/apriltags/src/GLineSegment2D.cc" "/home/barton/Documents/aptags/apriltags/pod-build/CMakeFiles/apriltags.dir/src/GLineSegment2D.cc.o"
+  "/home/barton/Documents/aptags/apriltags/src/Gaussian.cc" "/home/barton/Documents/aptags/apriltags/pod-build/CMakeFiles/apriltags.dir/src/Gaussian.cc.o"
+  "/home/barton/Documents/aptags/apriltags/src/GrayModel.cc" "/home/barton/Documents/aptags/apriltags/pod-build/CMakeFiles/apriltags.dir/src/GrayModel.cc.o"
+  "/home/barton/Documents/aptags/apriltags/src/Homography33.cc" "/home/barton/Documents/aptags/apriltags/pod-build/CMakeFiles/apriltags.dir/src/Homography33.cc.o"
+  "/home/barton/Documents/aptags/apriltags/src/MathUtil.cc" "/home/barton/Documents/aptags/apriltags/pod-build/CMakeFiles/apriltags.dir/src/MathUtil.cc.o"
+  "/home/barton/Documents/aptags/apriltags/src/Quad.cc" "/home/barton/Documents/aptags/apriltags/pod-build/CMakeFiles/apriltags.dir/src/Quad.cc.o"
+  "/home/barton/Documents/aptags/apriltags/src/Segment.cc" "/home/barton/Documents/aptags/apriltags/pod-build/CMakeFiles/apriltags.dir/src/Segment.cc.o"
+  "/home/barton/Documents/aptags/apriltags/src/TagDetection.cc" "/home/barton/Documents/aptags/apriltags/pod-build/CMakeFiles/apriltags.dir/src/TagDetection.cc.o"
+  "/home/barton/Documents/aptags/apriltags/src/TagDetector.cc" "/home/barton/Documents/aptags/apriltags/pod-build/CMakeFiles/apriltags.dir/src/TagDetector.cc.o"
+  "/home/barton/Documents/aptags/apriltags/src/TagFamily.cc" "/home/barton/Documents/aptags/apriltags/pod-build/CMakeFiles/apriltags.dir/src/TagFamily.cc.o"
+  "/home/barton/Documents/aptags/apriltags/src/UnionFindSimple.cc" "/home/barton/Documents/aptags/apriltags/pod-build/CMakeFiles/apriltags.dir/src/UnionFindSimple.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  "../build/include"
+  "../AprilTags"
+  "../."
+  "/opt/local/include"
+  "/usr/include/opencv"
+  "/usr/include/eigen3"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
