3b56c7f47c6f38f366fb3706cc5654f7419c2083
maquann <maquann@gmail.com>
2016-07-05 15:04:56 -0400

    testing

--- Apriltags.cpp
+++ Apriltags.cpp
@@ -68,6 +68,7 @@ void Apriltags::updatePosition()
 		cout << "ATAGS: " << c1 << aID << c2 << ax << c3 << ay << c4 << aroll << endl;
   		ax = 1000*ax;
     	ay = 1000*ay;
+    	aroll = -1*aroll;
 	
 		switch(aID)
 		{
