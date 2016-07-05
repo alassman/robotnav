106e308e93670f3105894b0c1a20089ff7ae3f0d
maquann <maquann@gmail.com>
2016-07-05 14:03:59 -0400

    testing

--- Apriltags.cpp
+++ Apriltags.cpp
@@ -95,6 +95,6 @@ void Apriltags::updatePosition()
 		mHeading = math_functions::unwrap(mHeading);
 	//	cout << "Apriltag ID:" << pID << " X: " << ax << " Y: " << ay << " Roll: " << aroll << endl;
 	
-	//	cout << "POSITION " << mpSensors->getName() << ": X: " <<mX << " Y: " << mY << " heading: " << math_functions::rad2deg(mHeading) << " speed: " << mSpeed << " rate: " << math_functions::rad2deg(mRate) << endl;
+	cout << "POSITION " << "Aptag Update " << " X: " <<mX << " Y: " << mY << " heading: " << math_functions::rad2deg(mHeading) << " speed: " << mSpeed << " rate: " << math_functions::rad2deg(mRate) << endl;
 	}
 }
