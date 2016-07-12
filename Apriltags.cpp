898df3566f1bc067796cd18c0211500d7de02dc6
maquann <maquann@gmail.com>
2016-07-12 16:27:27 -0400

    testing

--- Apriltags.cpp
+++ Apriltags.cpp
@@ -67,22 +67,22 @@ void Apriltags::updatePosition()
 			case 0:
 				//mHeading = -1*(3*PI/2 - aroll);
 				mX = 1620 - (ax*cos(-mHeading) - ay*sin(-mHeading)); //May have to flip mX like mY
-				mY = 0 - (ax*sin(-mHeading) + ay*cos(-mHeading));
+				mY = 3340 - (ax*sin(-mHeading) + ay*cos(-mHeading));
 				break;
 			case 1:
 				//mHeading = -1*(0 - aroll);
 				mX = 3360 - (ax*cos(-mHeading) - ay*sin(-mHeading));
-				mY = 1580 - (ax*sin(-mHeading) + ay*cos(-mHeading));
+				mY = 1770 - (ax*sin(-mHeading) + ay*cos(-mHeading));
 				break;
 			case 2:
 				//mHeading = -1*(PI/2 - aroll);
 				mX = 1750 - (ax*cos(-mHeading) - ay*sin(-mHeading));
-				mY = 3340 - (ax*sin(-mHeading) + ay*cos(-mHeading));
+				mY = -20 - (ax*sin(-mHeading) + ay*cos(-mHeading));
 				break;
 			case 3:
 				//mHeading = -1*(PI - aroll);
-				mX = 0 - (ax*cos(-mHeading) - ay*sin(-mHeading));
-				mY = 1740 - (ax*sin(-mHeading) + ay*cos(-mHeading));
+				mX = -20 - (ax*cos(-mHeading) - ay*sin(-mHeading));
+				mY = 1620 - (ax*sin(-mHeading) + ay*cos(-mHeading));
 				break;
 		}
 		//mHeading = math_functions::unwrap(mHeading);
